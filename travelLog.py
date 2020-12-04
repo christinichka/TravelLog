cb14742fc9adbea07903d5560aab6f884378b8d0
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2020-12-04 12:26:16 -0600

    Add files via upload

---
+++ travelLog.py
@@ -0,0 +1,48 @@
+travel_log = [
+	{
+	"counrty": ["Russia"], 
+	"cities visited": ["St. Petersburg", "Moscow", "Sochi"], 
+	"total_visits": 2
+	},
+
+	{
+	"counrty": ["Estonia"], 
+	"cities visited": ["Tallin"], 
+	"total_visits": 1
+	},
+
+	{
+	"counrty": ["Latvia"], 
+	"cities visited": ["Riga"], 
+	"total_visits": 1
+	},
+
+	{
+	"counrty": ["Canada"], 
+	"cities visited": ["Vancouver"], 
+	"total_visits": 1
+	},
+
+	{
+	"counrty": ["The Netherlands"], 
+	"cities visited": ["Amsterdam"], 
+	"total_visits": 1
+	},
+
+	{
+	"counrty": ["Italy"], 
+	"cities visited": ["Gaeta", "Rome", "Amalfi", "Naples"], 
+	"total_visits": 1
+	},
+
+	{
+	"counrty": ["Czech Republic"], 
+	"cities visited": ["Prague"], 
+	"total_visits": 4},
+
+	{
+	"counrty": ["Mexico"], 
+	"cities visited": ["Tijuana"], 
+	"total_visits": 1
+	},
+]
